599b24842405012efe98e0907cd1d9f0404a446c
Elena Vergou <elenberg@ionio.gr>
2021-03-21 15:30:08 +0000

    Download match ID and match data from Open Dota API

---
+++ data/getMatchData.py
@@ -0,0 +1,29 @@
+import requests
+import pandas as pd
+import json
+
+
+# read in match id data
+matchID_df = pd.read_csv('promatchID.csv')
+matchID_list = matchID_df['match_id'].to_list()
+total_matches_df = pd.DataFrame()
+
+for i in matchID_list[:2]:
+    match = requests.get("https://api.opendota.com/api/matches/{}".format(i))
+    match = json.loads(match.text)
+    match_df = pd.json_normalize(match)
+    match_df = match_df[['first_blood_time',
+                         'game_mode',
+                         'human_players',
+                         'objectives',
+                         'version',
+                         'patch',
+                         'players',
+                         'objectives',
+                         'radiant_gold_adv',
+                         'radiant_xp_adv',
+                         'skill',
+                         'region']].copy()
+    total_matches_df = total_matches_df.append(match_df, ignore_index=True)
+
+total_matches_df.to_csv('matchData.csv', index=False)
