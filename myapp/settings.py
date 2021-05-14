1f03609007525924f334d43f7391392b222261d4
Elena Vergou <elenberg@ionio.gr>
2021-05-14 04:13:32 +0100

    updating heroku configs

--- myapp/settings.py
+++ myapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,3 +129,5 @@ MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'dotaStatsApp', 'static'),
 ]
+
+django_heroku.settings(locals())
